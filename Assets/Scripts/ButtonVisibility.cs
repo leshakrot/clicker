94eb699245d46cbf5e54b742085d93791806d7d2
leshakrot <85496308+leshakrot@users.noreply.github.com>
2023-08-09 17:13:52 +0300

    fix

--- Assets/Scripts/ButtonVisibility.cs
+++ Assets/Scripts/ButtonVisibility.cs
@@ -24,13 +24,23 @@ public class ButtonVisibility : MonoBehaviour
 
     public void OnButtonClick(Button button)
     {
+        if(button.gameObject.GetComponent<PurchaseButton>() == null)
+        {
+            if (2500 <= Bank.instance.GetAmountCurrency())
+            {
+                string key = "Button" + button.name;
+                button.gameObject.SetActive(false);
+                PlayerPrefs.SetInt(key, BoolToInt(button.gameObject.activeSelf));
+                return;
+            }
+        }
+
         _purchaseButton = button.gameObject.GetComponent<PurchaseButton>();
         if (_purchaseButton.GetCharacter().GetPrice() <= Bank.instance.GetAmountCurrency())
         { 
             string key = "Button" + button.name;
             button.gameObject.SetActive(false);
             PlayerPrefs.SetInt(key, BoolToInt(button.gameObject.activeSelf));
-            Debug.Log("");
         }
     }
 
